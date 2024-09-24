7af4b231e1fcae00f6f6e65fa939dcd3299eb892
madoka pc <askdjfh@slkd.com>
2024-09-24 11:21:31 +0500

    day 2 inefficient solution

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -9,24 +9,37 @@ pub fn part1(input: &str) -> i32 {
 		.map(|chunk| -> i32 {
 			chunk
 				.lines()
-				.map(|a| {
-					// println!("{a}");
-					a.trim().parse::<i32>().unwrap()
-				})
+				.map(|line| line.trim().parse::<i32>().unwrap())
+				// .inspect(|f| println!("parsed: {:?}", f))
 				.sum::<i32>()
 		})
+		// .inspect(|f| println!("Chunk sum: {:?}\n\n", f))
 		.max()
 		.unwrap()
 }
 
 pub fn part2(input: &str) -> i32 {
-	todo!()
+	let mut temp: Vec<i32> = input
+		.trim()
+		.split("\n\n")
+		.map(|chunk| -> i32 {
+			chunk
+				.lines()
+				.map(|line| line.trim().parse::<i32>().unwrap())
+				.sum::<i32>()
+		})
+		.collect::<Vec<i32>>();
+
+	temp.sort_by(|a, b| b.cmp(a));
+	let sum = temp[0..=2].iter().sum();
+
+	sum
 }
 
 fn main() {
-	println!("{}", part1(input::inputs::INPUT_PART1));
+	println!("Part 1 Solution: {}", part1(input::inputs::INPUT_PART1));
 	// usually they have two different inputs but we in this case they didnt idk
-	println!("{}", part2(input::inputs::INPUT_PART1));
+	println!("Part 2 Solution: {}", part2(input::inputs::INPUT_PART1));
 }
 
 #[cfg(test)]
@@ -37,7 +50,7 @@ mod tests {
 	fn part1_test() {
 		assert_eq!(
 			part1(
-				"1000\n2000\n3000\n\n4000\n5000\n6000\n\n7000\n8000\n9000\n\n10000"
+				"1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000"
 			),
 			24000
 		);
@@ -47,7 +60,7 @@ mod tests {
 	fn part2_test() {
 		assert_eq!(
 			part2(
-				"1000\n2000\n3000\n\n4000\n5000\n6000\n\n7000\n8000\n9000\n\n10000"
+				"1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000"
 			),
 			45000
 		)
