2320c76d3b56f0bb6f944171b4f0a8db41bd421b
madoka pc <askdjfh@slkd.com>
2024-09-25 09:06:43 +0500

    did day 2 part 1

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,8 +2,68 @@
 
 mod input;
 
-fn part1(input: &str) -> i32 {
-	todo!()
+// this might be a bit overkill
+enum RPS {
+	Rock,
+	Paper,
+	Sissors,
+}
+impl RPS {
+	fn find_score(&self, opponent: &RPS) -> u32 {
+		match (self, opponent) {
+			(RPS::Rock, RPS::Rock) => 3 + 1,
+			(RPS::Rock, RPS::Paper) => 0 + 1,
+			(RPS::Rock, RPS::Sissors) => 6 + 1,
+			(RPS::Paper, RPS::Rock) => 6 + 2,
+			(RPS::Paper, RPS::Paper) => 3 + 2,
+			(RPS::Paper, RPS::Sissors) => 0 + 2,
+			(RPS::Sissors, RPS::Rock) => 0 + 3,
+			(RPS::Sissors, RPS::Paper) => 6 + 3,
+			(RPS::Sissors, RPS::Sissors) => 3 + 3,
+		}
+	}
+}
+
+#[test]
+fn test_rps_scores() {
+	assert_eq!(RPS::Rock.find_score(&RPS::Rock), 4);
+	assert_eq!(RPS::Rock.find_score(&RPS::Paper), 1);
+	assert_eq!(RPS::Rock.find_score(&RPS::Sissors), 7);
+	assert_eq!(RPS::Paper.find_score(&RPS::Rock), 8);
+	assert_eq!(RPS::Paper.find_score(&RPS::Paper), 5);
+	assert_eq!(RPS::Paper.find_score(&RPS::Sissors), 2);
+	assert_eq!(RPS::Sissors.find_score(&RPS::Rock), 3);
+	assert_eq!(RPS::Sissors.find_score(&RPS::Paper), 9);
+	assert_eq!(RPS::Sissors.find_score(&RPS::Sissors), 6);
+}
+
+
+fn part1(input: &str) -> u32 {
+	input
+		.trim()
+		.lines()
+		.map(|r#match| {
+			let mut battle = r#match.split(" ");
+
+			let opponent = match battle.next().unwrap() {
+				"A" => RPS::Rock,
+				"B" => RPS::Paper,
+				"C" => RPS::Sissors,
+				_ => panic!("something fucked up idk"),
+			};
+
+			let player = match battle.next().unwrap() {
+				"X" => RPS::Rock,
+				"Y" => RPS::Paper,
+				"Z" => RPS::Sissors,
+				_ => panic!("something fucked up idk"),
+			};
+
+			player.find_score(&opponent)
+		})
+		.inspect(|f| {
+			println!("{:?}", f);
+		}).sum()
 }
 
 fn part2(input: &str) -> i32 {
@@ -21,7 +81,14 @@ pub mod tests {
 
 	#[test]
 	fn part1_test() {
-		assert_eq!(part1(""), 0)
+		assert_eq!(
+			part1(
+				"A Y
+B X
+C Z"
+			),
+			15
+		)
 	}
 
 	#[test]
