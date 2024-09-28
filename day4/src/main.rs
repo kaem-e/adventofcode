9d473ffc80e21ee16cdb0cb7fe9855f7f9343d2f
madoka pc <askdjfh@slkd.com>
2024-09-28 17:01:42 +0500

    did day2 bad

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -6,33 +6,47 @@ fn part1(input: &str) -> u32 {
 	input
 		.lines()
 		.filter(|&line| {
-			println!("in line: {}", &line);
 			let mut line = line.split(",");
 			let mut compute = |line: &mut std::str::Split<'_, &str>| {
 				line
-					.inspect(|f| println!("about to process line: {}", f))
 					.next()
 					.unwrap()
 					.split("-")
-					.inspect(|&f| println!("about to process: {}", f))
 					.map(|num| num.parse::<u32>().unwrap())
 					.collect_tuple()
 					.unwrap()
 			};
 			let (f_start, f_end) = compute(&mut line);
 			let (s_start, s_end) = compute(&mut line);
-			println!("first: {:?}", (f_start, f_end));
-			println!("second: {:?}", (s_start, s_end));
 			let res = ((f_start <= s_start && f_end >= s_end)
 				|| (s_start <= f_start && s_end >= f_end));
-			println!("returned: {:?}\n", res);
 			res
 		})
 		.count() as u32
 }
 
 fn part2(input: &str) -> u32 {
-	todo!()
+	input
+		.lines()
+		.filter(|&line| {
+			let mut line = line.split(",");
+			let mut compute = |line: &mut std::str::Split<'_, &str>| {
+				line
+					.next()
+					.unwrap()
+					.split("-")
+					.map(|num| num.parse::<u32>().unwrap())
+					.collect_tuple()
+					.unwrap()
+			};
+			let (f_start, f_end) = compute(&mut line);
+			let (s_start, s_end) = compute(&mut line);
+
+			let res = (f_start <= s_end && f_end >= s_start)
+				|| (s_start <= f_end && s_end >= f_start);
+			res
+		})
+		.count() as u32
 }
 
 fn main() {
@@ -61,6 +75,16 @@ pub mod tests {
 
 	#[test]
 	fn part2_test() {
-		assert_eq!(part2(""), 0)
+		assert_eq!(
+			part2(
+				"2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"
+			),
+			4
+		)
 	}
 }
