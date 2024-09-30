2cd6240cc55e0f8c2b231048c739ba44010f2273
madoka pc <askdjfh@slkd.com>
2024-09-30 17:55:22 +0500

    day7 init

---
+++ day7/src/main.rs
@@ -0,0 +1,30 @@
+#![allow(unused)]
+mod input;
+
+fn part1(input: &str) -> u32 {
+	todo!()
+}
+
+fn part2(input: &str) -> u32 {
+	todo!()
+}
+
+fn main() {
+	println!("Part 1 Answer: {:?}", part1(input::INPUT));
+	println!("Part 2 Answer: {:?}", part2(input::INPUT));
+}
+
+#[cfg(test)]
+pub mod tests {
+	use super::*;
+
+	#[test]
+	fn part1_test() {
+		assert_eq!(part1(""), 0)
+	}
+
+	#[test]
+	fn part2_test() {
+		assert_eq!(part2(""), 0)
+	}
+}
