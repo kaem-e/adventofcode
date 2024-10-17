9e9cad3fb31ff65a4b492cc82a88fd6e21b5b62e
madoka pc <askdjfh@slkd.com>
2024-10-17 16:40:25 +0500

    cleaned up part 2 & switched to using itertools for part2
    mostly just followed along with
    'https://fasterthanli.me/series/advent-of-code-2022/part-1' because i
    wanted to learn how to use itertools properly
    
    i really dont like using iterators so far ngl, theyre elegant as hell
    when youre actually reading code because youc an just read them as is
    but writing code in them is quite painful (i might just be doing them
    wrongly though so far lmao)

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,10 +1,13 @@
-pub mod input;
+#![allow(unused)]
+use itertools::Itertools;
+use std::cmp::Reverse;
+mod input;
 
 fn part1(input: &str) -> i32 {
 	input
 		.split("\n\n")
-		.map(|chunk| -> i32 {
-			chunk
+		.map(|group| -> i32 {
+			group
 				.lines()
 				.map(|line| line.parse::<i32>().unwrap())
 				.sum::<i32>()
@@ -14,32 +17,18 @@ fn part1(input: &str) -> i32 {
 }
 
 fn part2(input: &str) -> u32 {
-	let mut max_nums: [u32; 3] = [u32::MIN; 3];
 	input
-		.trim()
-		.split("\n\n")
-		// .inspect(|f| println!("About to sum: \n{f}"))
-		.for_each(|chunk| {
-			let sum = chunk
-				.lines()
-				.map(|line| line.trim().parse::<u32>().unwrap())
-				.sum::<u32>();
-			// println!("Result: {sum}\n");
-			if max_nums[0] <= sum {
-				max_nums[0] = sum;
-				// this feels really inefficient ngl,
-				// thisll have to be called for every element
-				// but atleast its only being done when a new element is added so
-				max_nums.sort();
-			}
-		});
-
-	max_nums.iter().sum()
+		.lines()
+		.map(|v| v.parse::<u32>().ok())
+		.batching(|it| it.map_while(|x| x).sum1::<u32>())
+		.map(Reverse)
+		.k_smallest(3)
+		.map(|x| x.0)
+		.sum::<u32>()
 }
 
 fn main() {
 	println!("Part 1 Solution: {}", part1(input::inputs::INPUT_PART1));
-	// usually they have two different inputs but we in this case they didnt idk
 	println!("Part 2 Solution: {}", part2(input::inputs::INPUT_PART1));
 }
 
