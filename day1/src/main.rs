4d380632da591e7eedf22c6ddcb2ea3cdb92c99a
madoka pc <askdjfh@slkd.com>
2024-10-25 23:46:58 +0500

    added v old 2023 solution (need to redo this)

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,8 +2,35 @@
 
 mod input;
 
-fn part1(input: &str) -> i32 {
-	todo!()
+use itertools::Itertools;
+
+fn part1(input: &str) -> u32 {
+	let input = input
+		.lines()
+		.map(|x| {
+			x.chars()
+				.filter_map(|f| f.to_digit(10))
+				.collect::<Vec<u32>>()
+		})
+		.collect::<Vec<Vec<u32>>>();
+
+	let joined = input
+		.into_iter()
+		.map(|x| {
+			let joined = x.first().unwrap() * 10
+				+ match x.last() {
+					Some(last) => last,
+					None => x.first().unwrap(),
+				};
+			joined
+		})
+		.collect::<Vec<u32>>();
+
+	let sum = joined.into_iter().sum::<u32>();
+
+	return sum;
+	// VERY old solution lmao need to redo this after 2022
+	// ngl i could probably make this way more efficient but eh
 }
 
 fn part2(input: &str) -> i32 {
@@ -21,11 +48,27 @@ pub mod tests {
 
 	#[test]
 	fn part1_test() {
-		assert_eq!(part1(""), 0)
+		assert_eq!(
+			part1(
+				"1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"
+			),
+			142
+		)
 	}
 
 	#[test]
 	fn part2_test() {
-		assert_eq!(part2(""), 0)
+		assert_eq!(
+			part2(
+				"1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"
+			),
+			0
+		)
 	}
 }
